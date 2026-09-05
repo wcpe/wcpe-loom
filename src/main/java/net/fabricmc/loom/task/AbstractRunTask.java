wcpe/fabric-loom

fix: 延迟 AbstractRunTask 的 excludedLibraryPaths 解析以兼容 Gradle 9
92d02463 为配置缓存兼容把 config.map() 懒加载改为配置期急切物化，
但 RuntimeLibraries.getExcludedLibraryPaths() 会解析 minecraftClientRuntimeLibraries
配置，在构造函数中调用触发 Gradle 9 的 unsafe-resolution guard，
导致 runGameTest 等任务在任务创建阶段直接失败。

修复方案：
- RuntimeLibraries 新增接受 String environment 的重载，解耦对 RunConfiguration 的依赖
- AbstractRunTask 构造函数中 excludedLibraryPaths 改用 Provider 延迟到执行时解析
- 仅捕获纯数据快照（String），不捕获 Project 或 RunConfiguration，保持配置缓存兼容

src/main/java/net/fabricmc/loom/task/AbstractRunTask.java
@@ -48,6 +48,7 @@ import org.gradle.api.file.FileCollection;
 import org.gradle.api.provider.ListProperty;
 import org.gradle.api.provider.MapProperty;
 import org.gradle.api.provider.Property;
+import org.gradle.api.provider.Provider;
 import org.gradle.api.provider.ProviderFactory;
 import org.gradle.api.specs.Spec;
 import org.gradle.api.tasks.Classpath;
@@ -138,14 +139,23 @@ public abstract class AbstractRunTask extends JavaExec {
 		final Map<String, Object> envVarsSnapshot = Map.copyOf(runConfig.getEnvironmentVars().get());
 		final List<String> jvmArgsSnapshot = List.copyOf(runConfig.getJvmArguments().get());
 		final String environmentSnapshot = runConfig.getRuntimeEnvironment().get();
-		final List<String> excludedLibraryPathsSnapshot = RuntimeLibraries.getExcludedLibraryPaths(getProject(), runConfig);
+		final String sourceSetNameSnapshot = runConfig.getSourceSet().get();
 		final String configNameSnapshot = RunConfigUtils.getDisplayName(runConfig, getProject());
 
-		getInternalClasspath().from(SourceSetHelper.getSourceSetByName(runConfig.getSourceSet().get(), getProject()).getRuntimeClasspath()
-				.filter(new LibraryFilter(
-						excludedLibraryPathsSnapshot,
-						configNameSnapshot)
-				));
+		// excludedLibraryPaths 需要解析 minecraftClientRuntimeLibraries 配置，不能在配置期调用
+		// （Gradle 9 的 unsafe-resolution guard 会拦截跨 included-build 锁边界的配置解析）。
+		// 用 Provider 延迟到执行时解析，仅捕获纯数据快照（environmentSnapshot / sourceSetNameSnapshot /
+		// configNameSnapshot），不捕获 Project 或 RunConfiguration，保持配置缓存兼容。
+		final String environmentSnapshotForFilter = environmentSnapshot;
+		final Provider<List<String>> excludedLibraryPathsProvider = getProviders().provider(() ->
+				RuntimeLibraries.getExcludedLibraryPaths(getProject(), environmentSnapshotForFilter));
+
+		getInternalClasspath().from(getProviders().provider(() ->
+				SourceSetHelper.getSourceSetByName(sourceSetNameSnapshot, getProject()).getRuntimeClasspath()
+						.filter(new LibraryFilter(
+								excludedLibraryPathsProvider.get(),
+								configNameSnapshot)
+						)));
 
 		getArgumentProviders().add(new CommandLineArgumentProvider() {
 			@Override

wcpe/wcpe-loom

src/main/java/net/fabricmc/loom/task/AbstractRunTask.java
@@ -48,6 +48,7 @@ import org.gradle.api.file.FileCollection;
 import org.gradle.api.provider.ListProperty;
 import org.gradle.api.provider.MapProperty;
 import org.gradle.api.provider.Property;
+import org.gradle.api.provider.Provider;
 import org.gradle.api.provider.ProviderFactory;
 import org.gradle.api.specs.Spec;
 import org.gradle.api.tasks.Input;
@@ -64,6 +65,7 @@ import org.slf4j.LoggerFactory;
 
 import net.fabricmc.loom.LoomGradleExtension;
 import net.fabricmc.loom.configuration.ide.RunConfig;
+import net.fabricmc.loom.configuration.ide.RuntimeLibraries;
 import net.fabricmc.loom.task.prod.TracyCapture;
 import net.fabricmc.loom.util.Constants;
 import net.fabricmc.loom.util.Platform;
@@ -139,12 +141,21 @@ public abstract class AbstractRunTask extends JavaExec {
 		final String runDirSnapshot = runConfig.runDir;
 		final Map<String, Object> envVarsSnapshot = Map.copyOf(runConfig.environmentVariables);
 		final String environmentSnapshot = runConfig.environment;
-		final List<String> excludedLibraryPathsSnapshot = runConfig.getExcludedLibraryPaths(getProject());
 		final String configNameSnapshot = runConfig.configName;
 		final String nameSnapshot = runConfig.name;
 
-		getInternalClasspath().from(runConfig.sourceSet.getRuntimeClasspath()
-				.filter(new LibraryFilter(excludedLibraryPathsSnapshot, configNameSnapshot)));
+		// excludedLibraryPaths 需要解析 minecraftClientRuntimeLibraries 配置，不能在配置期调用
+		// （Gradle 9 的 unsafe-resolution guard 会拦截跨 included-build 锁边界的配置解析）。
+		// 用 Provider 延迟到执行时解析，仅捕获纯数据快照（environmentSnapshot），保持配置缓存兼容。
+		final Provider<List<String>> excludedLibraryPathsProvider = getProviders().provider(() ->
+				RuntimeLibraries.getExcludedLibraryPaths(getProject(), environmentSnapshot));
+
+		getInternalClasspath().from(getProviders().provider(() ->
+				runConfig.sourceSet.getRuntimeClasspath()
+						.filter(new LibraryFilter(
+								excludedLibraryPathsProvider.get(),
+								configNameSnapshot)
+						)));
 
 		getArgumentProviders().add(new CommandLineArgumentProvider() {
 			@Override